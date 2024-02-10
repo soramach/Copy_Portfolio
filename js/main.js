fe7ac51a4e2af61ede80389011343ea732ed5fab
soramach <soram4ch@gmail.com>
2024-02-10 10:31:18 +0900

    ✨ qaセクションの追加

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(".qa-item-title").on("click", function () {
+  //タイトル要素をクリックしたら
+  var findElm = $(this).next(".qa-item-answer"); //直後のアコーディオンを行うエリアを取得し
+  $(findElm).slideToggle(); //アコーディオンの上下動作
+
+  if ($(this).hasClass("close")) {
+    //タイトル要素にクラス名closeがあれば
+    $(this).removeClass("close"); //クラス名を除去し
+  } else {
+    //それ以外は
+    $(this).addClass("close"); //クラス名closeを付与
+  }
+});
